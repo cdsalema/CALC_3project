a2fce822e810e287c155bb9889d83b32ba174094
cdsalema <conradod.salema@gmail.com>
2023-02-15 09:28:08 -0300

    feat - arrow function rules

--- assets/scripts/calculadora.js
+++ assets/scripts/calculadora.js
@@ -1,4 +1,4 @@
-
+/**
 function insert(num) {
     const numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
@@ -14,10 +14,18 @@ function insert(num) {
   }
   
   function calcular() {
-    const resultado = document.getElementById('resultado').innerHTML;
+    let resultado = document.getElementById('resultado').innerHTML;
     if (resultado) {
       document.getElementById('resultado').innerHTML = eval(resultado);
     } else {
       document.getElementById('resultado').innerHTML = 'null';
     }
-  }
+  }**/
+
+const insert = num => document.getElementById('resultado').innerHTML += num;
+const clean = () => document.getElementById('resultado').innerHTML = '';
+const back = () => document.getElementById('resultado').innerHTML = document.getElementById('resultado').innerHTML.slice(0, -1);
+const calcular = () => {
+const resultado = document.getElementById('resultado').innerHTML;
+document.getElementById('resultado').innerHTML = resultado ? eval(resultado) : 'null';
+}
