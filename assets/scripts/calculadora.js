6355f8e5f56a0b3665848bb7dbfa20374f172a4b
cdsalema <conradod.salema@gmail.com>
2023-02-15 02:04:06 -0300

    feat - alterando VAR > const

--- assets/scripts/calculadora.js
+++ assets/scripts/calculadora.js
@@ -1,4 +1,4 @@
-function insert(num)
+/**function insert(num)
 {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
@@ -23,4 +23,26 @@ function calcular()
     {
         document.getElementById('resultado').innerHTML = "Nada..."
     }
-}
+}**/
+function insert(num) {
+    const numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+  }
+  
+  function clean() {
+    document.getElementById('resultado').innerHTML = '';
+  }
+  
+  function back() {
+    const resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.slice(0, -1);
+  }
+  
+  function calcular() {
+    const resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+      document.getElementById('resultado').innerHTML = eval(resultado);
+    } else {
+      document.getElementById('resultado').innerHTML = 'Nothing here...';
+    }
+  }
