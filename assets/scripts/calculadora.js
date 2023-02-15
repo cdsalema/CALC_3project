b6173538758e8acd5466e9354d669cf3a2d69d67
cdsalema <conradod.salema@gmail.com>
2023-02-15 02:06:43 -0300

    docs - retirada de comentarios desnecessarios.

--- assets/scripts/calculadora.js
+++ assets/scripts/calculadora.js
@@ -1,29 +1,4 @@
-/**function insert(num)
-{
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
-}
-function clean()
-{
-    document.getElementById('resultado').innerHTML = "";
-}
-function back()
-{
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
-}
-function calcular()
-{
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado);
-    }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "Nada..."
-    }
-}**/
+
 function insert(num) {
     const numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
@@ -43,6 +18,6 @@ function insert(num) {
     if (resultado) {
       document.getElementById('resultado').innerHTML = eval(resultado);
     } else {
-      document.getElementById('resultado').innerHTML = 'Nothing here...';
+      document.getElementById('resultado').innerHTML = 'null';
     }
   }
