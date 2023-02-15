c1867a282a820861b5bcd386aa2c97620d6a7015
cdsalema <conradod.salema@gmail.com>
2023-02-15 09:40:50 -0300

    docs - indentacao

--- assets/scripts/calculadora.js
+++ assets/scripts/calculadora.js
@@ -25,7 +25,7 @@ function insert(num) {
 const insert = num => document.getElementById('resultado').innerHTML += num;
 const clean = () => document.getElementById('resultado').innerHTML = '';
 const back = () => document.getElementById('resultado').innerHTML = document.getElementById('resultado').innerHTML.slice(0, -1);
-const calcular = () => {
+const calcular = () =>  {
 const resultado = document.getElementById('resultado').innerHTML;
 document.getElementById('resultado').innerHTML = resultado ? eval(resultado) : 'null';
 }
