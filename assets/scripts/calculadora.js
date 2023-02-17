1eab04a9c381097890518519f432545ab9dfaff0
cdsalema <conradod.salema@gmail.com>
2023-02-17 17:34:33 -0300

    docs -  identanção e armazenando script original

--- assets/scripts/calculadora.js
+++ assets/scripts/calculadora.js
@@ -1,27 +1,3 @@
-/**
-function insert(num) {
-    const numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
-  }
-  
-  function clean() {
-    document.getElementById('resultado').innerHTML = '';
-  }
-  
-  function back() {
-    const resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.slice(0, -1);
-  }
-  
-  function calcular() {
-    let resultado = document.getElementById('resultado').innerHTML;
-    if (resultado) {
-      document.getElementById('resultado').innerHTML = eval(resultado);
-    } else {
-      document.getElementById('resultado').innerHTML = 'null';
-    }
-  }**/
-
 const insert = num => document.getElementById('resultado').innerHTML += num;
 const clean = () => document.getElementById('resultado').innerHTML = '';
 const back = () => document.getElementById('resultado').innerHTML = document.getElementById('resultado').innerHTML.slice(0, -1);
---
+++ assets/scripts/scriptOriginal.js
@@ -0,0 +1,23 @@
+/**
+function insert(num) {
+    const numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+  }
+  
+  function clean() {
+    document.getElementById('resultado').innerHTML = '';
+  }
+  
+  function back() {
+    const resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.slice(0, -1);
+  }
+  
+  function calcular() {
+    let resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+      document.getElementById('resultado').innerHTML = eval(resultado);
+    } else {
+      document.getElementById('resultado').innerHTML = 'null';
+    }
+  }**/
